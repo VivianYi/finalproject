911255ddbc90cc52ef430645ea6a3b70a496812c
VivianYi <31659992+VivianYi@users.noreply.github.com>
2017-11-27 15:04:23 -0500

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    var canvas = document.getElementById("myCanvas")
+    var ctx = canvas.getContext("2d");
+    ctx.font = "30px Arial";
+    ctx.textAlign = "center";
+    ctx.fillText("Hello World", canvas.width/2, canvas.height/2);
+});
